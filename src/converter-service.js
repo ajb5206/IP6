202b4947a402cfb689a74d20b69d09483709558e
Alex Bertotto <ajb5206@gmail.com>
2021-08-14 12:38:25 -0700

    seperate js logic

---
+++ src/converter-service.js
@@ -0,0 +1,18 @@
+export default class ConverterService {
+  static convert(pair1, pair2) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${pair1}/${pair2}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    }); 
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,40 +2,31 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-$;
+import ConverterService from './converter-service.js';
 
 process.env.API_KEY;
+
+function clearFields() {  
+  $('#currency1').val('');
+  $('#currency2').val('');
+  $('#amount').val('');
+}
 
 $(document).ready(function() {
   $('#currencyConvert').click(function() {
     let pair1 = $('#currency1').val();
     let pair2 = $('#currency2').val();
     const money = $('#amount').val();
-    $('#currency1').val('');
-    $('#currency2').val('');
-    $('#amount').val('');
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${pair1}/${pair2}`;
-
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-
+    clearFields();
+    let promise = ConverterService.convert(pair1, pair2);
+    
     promise.then(function(response) {
       const responseText = JSON.parse(response);
       $('.showConverted').text(`The conversion rate is ${responseText.conversion_rate} and the total converted amount 
 				is ${money * responseText.conversion_rate}`);
-    }, function(error) {
-      $('.showError').text(`The error is ${error}.`);
+    }, function(response) {
+      let errorText = JSON.parse(response);
+      $('.showError').text(`There is an ${errorText.result} due to ${errorText["error-type"]}.`);
     });
   });
 });
