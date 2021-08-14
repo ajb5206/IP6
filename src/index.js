d2d4d9880ad9ac9bd8e6c7b4f8d6c6b18bb4495d
Alex Bertotto <ajb5206@gmail.com>
2021-08-14 11:40:34 -0700

    add promises to api request

--- src/index.js
+++ src/index.js
@@ -4,35 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 $;
 
-//const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/EUR/GBP`
-// or	const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${pair1}/${pair2}`
-//      https://v6.exchangerate-api.com/v6/YOUR-API-KEY/latest/USD
-
 process.env.API_KEY;
 
 $(document).ready(function() {
   $('#currencyConvert').click(function() {
     let pair1 = $('#currency1').val();
-		let pair2 = $('#currency2').val();
-		const money = $('#amount').val();
-    
-		let request = new XMLHttpRequest();
-    const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${pair1}/${pair2}`;
-		
-    request.onreadystatechange = function() {
-      if (this.readyState === 4 && this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
+    let pair2 = $('#currency2').val();
+    const money = $('#amount').val();
+    $('#currency1').val('');
+    $('#currency2').val('');
+    $('#amount').val('');
 
-    request.open("GET", url, true);
-    request.send();
-		
-    function getElements(response) {
-      let exchangeRate = response.conversion_rate;
-      $('.showConverted').text(`The conversion rate is ${exchangeRate} and the total is ${money * exchangeRate}`);
-      //$('.showError').text(`The error is ${response.conversion_rate}.`);
-    }
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${pair1}/${pair2}`;
+
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+
+    promise.then(function(response) {
+      const responseText = JSON.parse(response);
+      $('.showConverted').text(`The conversion rate is ${responseText.conversion_rate} and the total converted amount 
+				is ${money * responseText.conversion_rate}`);
+    }, function(error) {
+      $('.showError').text(`The error is ${error}.`);
+    });
   });
 });
